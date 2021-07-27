c19262c7cd8d2808a78a2ca911245771007051c5
p0x0q <info.allegation@gmail.com>
2021-07-28 01:14:00 +0900

    fix

--- app/Http/Controllers/SignUpController.php
+++ app/Http/Controllers/SignUpController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class SignUpController extends Controller
 {
@@ -16,7 +17,7 @@ class SignUpController extends Controller
     public function store(Request $request)
     {
         $valid_dict = [
-            'user_id' => ['required', 'min:8', 'max:20'],
+            'user_id' => ['required', 'min:8', 'max:20', 'unique:user,user_id'],
             'password' => ['required', 'min:8', 'max:20'],
         ];
 
@@ -27,6 +28,15 @@ class SignUpController extends Controller
             ], 400);
         }
 
+        if (User::where('user_id', $request->user)) {
+            $validator = Validator::make($request->all(), $valid_dict);
+        }
+        if ($validator->fails()) {
+            return response([
+                "message" => "Account creation failed",
+                "cause" => "error",
+            ], 400);
+        }
         $request->validate($valid_dict);
         $data = $request->only(array_keys($valid_dict));
         $data['nickname'] = $request->user_id;
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 
 class UserController extends Controller
 {
@@ -77,22 +76,21 @@ class UserController extends Controller
     public function update(Request $request, $id)
     {
         $is_login = false;
-
-        $auth_header = $request->header('Authorization');
-        $auth = explode(":", base64_decode(str_replace("Basic ", "", $auth_header)));
-        $user_id = $auth[0];
-        $password = $auth[1];
-
-        $userinfo = User::where(['user_id' => $user_id, 'password' => $password]);
-        if (!$userinfo->exists()) {
-            return response(["message" => "Authentication Failed"], 401);
-        }
-        $is_login = $userinfo->exists();
-
-        Log::debug(['info' => $auth, 'login' => $is_login]);
+        // Log::debug(['info' => $auth, 'login' => $is_login]);
 
         try {
+            $auth_header = $request->header('Authorization');
+            $auth = explode(":", base64_decode(str_replace("Basic ", "", $auth_header)));
+            $user_id = $auth[0];
+            $password = $auth[1];
+
+            $userinfo = User::where(['user_id' => $user_id, 'password' => $password]);
+            if (!$userinfo->exists()) {
+                return response(["message" => "Authentication Failed"], 401);
+            }
+            $is_login = $userinfo->exists();
         } catch (Exception $e) {
+            return response(["message" => "Authentication Failed"], 401);
         }
 
         $valid_dict = [
